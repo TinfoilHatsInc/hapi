ff973c77272844a108d37a054aeaa953f6151a69
mskelo <matejskelo@gmail.com>
2017-12-05 11:30:35 +0100

    shit works now yo

--- core/hapi/RegistrationDBController.php
+++ core/hapi/RegistrationDBController.php
@@ -4,6 +4,8 @@ namespace core\hapi;
 
 use core\utils\ConfigReader;
 use mysqli;
+use core\rest\SuccessResponse;
+use core\rest\ErrorResponse;
 
 class RegistrationDBController
 {
@@ -29,21 +31,28 @@ class RegistrationDBController
     );
 
     $checkid = filter_var($checkid, FILTER_SANITIZE_STRING);
-    $stmt = $this->db->prepare("SELECT * from IDTable WHERE id = ?");
+    $stmt = $this->db->prepare("SELECT * from IDTable WHERE deviceid = ?");
     $stmt->bind_param('s', $checkid);
-    $this->result = $stmt->execute();#$this->db->query($stmt);
+    $stmt->execute();
+    $stmtResult = $stmt->get_result();
+    $this->result = [];
+    while($row = $stmtResult->fetch_array(MYSQLI_ASSOC)) {
+    	$this->result[] = $row;
+    }
     $this->db->close();
   }
 
   public function print(){
-  	if ($this->result->num_rows > 0) {
+  	if (count($this->result) > 0) {
+  	  (new SuccessResponse(SuccessResponse::HTTP_OK, $this->result))->send();
       echo "<table><tr><th>ID</th><th>User</th></tr>";
       // output data of each row
-      while ($row = $this->result->fetch_assoc()) {
-        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["user"] . "</td></tr>";
+      foreach ($this->result as $row) {
+        echo "<tr><td>" . $row["deviceid"] . "</td><td>" . $row["user"] . "</td></tr>";
       }
       echo "</table>";
     } else {
+      (new ErrorResponse(ErrorResponse::HTTP_NOT_FOUND, $this->result))->send();
       echo "0 results";
     }
   }

ed67d09ea372509f4045e787e6a3553c86abd054
mskelo <matejskelo@gmail.com>
2017-12-05 09:58:35 +0100

    small fixes

--- core/hapi/RegistrationDBController.php
+++ core/hapi/RegistrationDBController.php
@@ -31,15 +31,15 @@ class RegistrationDBController
     $checkid = filter_var($checkid, FILTER_SANITIZE_STRING);
     $stmt = $this->db->prepare("SELECT * from IDTable WHERE id = ?");
     $stmt->bind_param('s', $checkid);
-    this->$result = $this->db->query($stmt);
+    $this->result = $this->db->query($stmt);
     $this->db->close();
   }
 
   public function print(){
-  	if (this->$result->num_rows > 0) {
+  	if ($this->result->num_rows > 0) {
       echo "<table><tr><th>ID</th><th>User</th></tr>";
       // output data of each row
-      while ($row = this->$result->fetch_assoc()) {
+      while ($row = $this->result->fetch_assoc()) {
         echo "<tr><td>" . $row["id"] . "</td><td>" . $row["user"] . "</td></tr>";
       }
       echo "</table>";

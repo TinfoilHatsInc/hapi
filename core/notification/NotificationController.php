fb64323cf5a470edda417611c7ea424fb079ad7f
Martijn Bots <martijnbots2@gmail.com>
2018-01-12 11:01:20 +0100

    Userdetails from chub added

--- core/notification/NotificationController.php
+++ core/notification/NotificationController.php
@@ -3,6 +3,7 @@
 namespace core\notification;
 
 use core\database\PortalDBController;
+use core\exception\EntityNotFoundException;
 use core\rest\Response;
 
 class NotificationController
@@ -14,12 +15,18 @@ class NotificationController
    * @param string $subject
    * @param string $chubId
    * @return Response
+   * @throws EntityNotFoundException
    */
   public static function sendNotification($message, $subject, $chubId) {
     $portalDBController = new PortalDBController();
     $user = $portalDBController->getUserByChub($chubId);
-    $mailNotification = new MailNotification($message, $subject, $user['email']);
-    return $mailNotification->send();
+    if(!empty($user)) {
+      $user = $user[0];
+      $mailNotification = new MailNotification($message, $subject, $user['email']);
+      return $mailNotification->send();
+    } else {
+      throw new EntityNotFoundException('User not found.');
+    }
   }
 
 }
